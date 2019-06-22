2b7cb80ccafb1cef520c50d617695a1f71faf965
kaya123 <kaya.dahlke@gmail.com>
2019-06-22 17:25:00 +1000

    Retry fixing makeAction

--- game.c
+++ game.c
@@ -220,7 +220,7 @@ void makeAction (Game g, action a) {
     } else if (a.actionCode == BUILD_GO8) { 
         g->vertices[x][y].campus = currentPlayer + 3; //add to account for GO8
         g->players[currentPlayer-1]->numCampuses--; //remove campus as GO8 replaces campus
-        g->players[currentPlayer-1]->numGO8++;
+        g->players[currentPlayer-1]->numGO8s++;
         // remove studentType
         g->players[currentPlayer-1]->studentType[STUDENT_MJ] -= 2; //to account for cost of GO8
         g->players[currentPlayer-1]->studentType[STUDENT_MMONEY] -= 3;
